a06e9cafac157f685d269b7d918d5bd3c803a142
lukaszkoltun <lukasz.koltun01@gmail.com>
2023-04-29 19:52:14 +0200

    added color interval

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello from Nerdbord!')
+const box = document.querySelector(".box");
+const colors = ["#16dbf9", "#def916", "#16f921", "#f916f1"];
+let counter = 0;
+box.style.backgroundColor = colors[3];
+setInterval(() => {
+  box.style.backgroundColor = colors[counter];
+  counter++;
+  counter === 4 ? (counter = 0) : null;
+}, 3000);
